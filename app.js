c9714cd27e09b5ceffc08e549a3afbf7e14acc80
rebecatinnoco <104739229+rebecatinnoco@users.noreply.github.com>
2022-07-09 15:35:02 -0400

    commit #2

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const path = require('path');
+const auth = require('http-auth');
+// const basic = auth.basic({
+//     file: path.join(__dirname, './users.htpasswd'),
+// }) 
 const routes = require('./routes/index');
 
 const app = express();
@@ -10,5 +14,6 @@ app.set('view engine', 'pug');
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use('/', routes);
+app.use(express.static('public'));
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
+const Registration = mongoose.model('Registration');
+const path = require('path');
+const auth = require('http-auth');
+const basic = auth.basic({
+    file: path.join(__dirname, '../users.htpasswd'),
+}) 
+
+
 
 router.get('/', function(req,res) {
     res.render('form', { title: 'Registration form' });
 });
+router.get('/registrations', basic.check((req,res) => {
+    Registration.find()
+        .then((registrations) => {
+    res.render('index', { title: 'Listening registrations', registrations });
+})
+        .catch(() => { res.send('Sorry! Something is wrong.');});
+}));
 router.post('/', 
 [
     check('name')
@@ -18,7 +34,13 @@ function(req,res) {
     // console.log(req.body);
     const errors = validationResult(req);
     if (errors.isEmpty()) {
-        res.send('Thank you for your registration!');
+        const registration = new Registration(req.body);
+        registration.save()
+        .then(() => { res.send('Thank you for your registration!');} )
+        .catch((err) => {
+            console.log(err);
+            res.send('Sorry! Something is wrong.');
+        });
     } else {
       res.render('form', { 
           title: 'Registration form',
